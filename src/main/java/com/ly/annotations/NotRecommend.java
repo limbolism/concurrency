40fad53c5bd923df8f0cbbd0960664578f3a9775
ly <445671082@qq.com>
2019-02-21 12:21:12 +0800

    add annotations

---
+++ src/main/java/com/ly/annotations/NotRecommend.java
@@ -0,0 +1,17 @@
+package com.ly.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author limbo
+ *         Created on 2019/2/21.
+ *         用来标记不推荐的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+    String value() default "";
+}
